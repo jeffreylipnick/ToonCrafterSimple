e3ece653406822542774fff5366e689bb20c85a6
Jeffrey Lipnick <lipnickj@gmail.com>
2025-04-16 19:57:14 -0600

    Debug

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
+import numpy as np
+from PIL import Image
 from tooncrafter.i2v import Image2Video
 
 i2v = Image2Video("src/tooncrafter/new.yaml", result_dir="/tmp/tc-results", resolution='320_512', fp16=True)
 
-image = ... # e.g. numpy.asarray(PIL.Image.open(...))
+image1 = np.asarray(Image.open("~/assets/tooncrafter/74906_1462_frame1.png"))
+image2 = np.asarray(Image.open("~/assets/tooncrafter/74906_1462_frame3.png"))
 
-video_output_path = i2v.get_image(image, "an anime scene", image, steps=20, fs=10)
+video_output_path = i2v.get_image(image1, "an anime scene", image2, steps=5, fs=1)
